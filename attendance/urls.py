38644448963c14551532c19c3524d55c9a297939
Saumya <saumya.mishra@intelliatech.com>
2024-09-05 16:32:27 +0530

    attendane all features working fine

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('/search/', views.attendance_search, name='attendance_search'),
     path('/AIndex/', views.attendance_view, name='attendance'),
-    path('/attendanceList/', views.attendance_list, name='attendanceList'),
     path('/modify/', views.modify_attendance, name='modify_attendance'),
     path('/get_yearly_attendance_status/',views.get_yearly_attendance_status,name='get_yearly_attendance_status'),
 ]
